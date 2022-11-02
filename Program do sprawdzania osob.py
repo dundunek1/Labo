2cb7a2984ae6aafb612a599f15b75e30b3a5a2d3
dundunek1 <115711835+dundunek1@users.noreply.github.com>
2022-11-02 11:41:17 +0100

    Add files via upload
    ez XDDDDDDD

---
+++ Program do sprawdzania osob.py
@@ -0,0 +1,13 @@
+print('Cudowny program do sprawdzenia osoby')
+x = input('Podaj imie:')
+
+if len(x) == 4 or len(x) == 7:
+ print('jebany cwel falszywy')
+if len(x) == 6:
+ print('dziwka jebana')
+if len(x) == 8:
+ print('Cudowna osobka :3')
+if len(x) == 3:
+ print('ork')
+if len(x) == 5:
+ print('krul')
