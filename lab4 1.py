9f57fc0f89ea4601d35c786cb7de339aebfa9255
dundunek1 <115711835+dundunek1@users.noreply.github.com>
2022-11-03 15:05:49 +0100

    Add files via upload

---
+++ lab4 1.py
@@ -0,0 +1,6 @@
+lista_zakupow =['chleb', 'jajka', 'szynka', 'ser']
+print(lista_zakupow)
+print(lista_zakupow[0],lista_zakupow[1])
+lista_zakupow[2]='mleko'
+print(lista_zakupow)
+print(lista_zakupow[-2],lista_zakupow[-1])
---
+++ lab4 2.py
@@ -0,0 +1,7 @@
+zestaw_1=[]
+import random
+y = int(input("podaj liczbe elemtnow listy"))
+for i in range(y):
+    x = random.randint(1,10)
+    zestaw_1.append(x)
+print(zestaw_1)
